a1ed40acfea04637b756e6755fd006aa2a33ea99
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-06-19 08:55:09 -0300

    projeto final

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,6 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
 
   final TextEditingController controller = TextEditingController();
@@ -33,9 +32,97 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Buscar Filmes')),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                hintText: 'Digite o filme',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: pesquisar,
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            if (carregando) const CircularProgressIndicator(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filmes.length,
+                itemBuilder: (context, index) {
+                  final filme = filmes[index];
+                  return Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.only(bottom: 12),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                            filme.poster != 'N/A'
+                                ? filme.poster
+                                : 'https://via.placeholder.com/100',
+
+                            width: 100,
+                            height: 150,
+                            fit: BoxFit.cover,
+
+                            errorBuilder: (context, error, stackTrace) {
+                              return Container(
+                                width: 100,
+                                height: 150,
+                                color: Colors.grey[300],
+
+                                child: const Icon(
+                                  Icons.image_not_supported,
+                                  size: 40,
+                                ),
+                              );
+                            },
+                          ),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  filme.titulo,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text('Ano: ${filme.ano}'),
+                                const SizedBox(height: 10),
+                                Text(
+                                  filme.descricao,
+                                  maxLines: 5,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
